a37a11da0113e84d4f01f62a2af5d10b30535eab
Lucas Valcarce <lucasvalcarce003@gmail.com>
2026-03-03 09:45:12 -0400

    prueba api front

--- validaya frontend/src/services/api.js
+++ validaya frontend/src/services/api.js
@@ -1,6 +1,16 @@
 const API_BASE_URL = "http://localhost:8081";
 
-export const getExample = async () => {
-  const response = await fetch(`${API_BASE_URL}/el-endpoint`);
+export const getExample = async (email, password) => {
+  const response = await fetch(`${API_BASE_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: 'admin@validaya.com',
+      password: 'pass123'
+    })
+  });
+  
   return response.json();
 };
