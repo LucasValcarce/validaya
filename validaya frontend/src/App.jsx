578603c66ae78ada51e5bcd6ec62dc9f97a37fc7
ISabela <isabelaortizr11@gmail.com>
2026-03-16 08:40:54 -0400

    instalacion analytics

--- validaya frontend/src/App.jsx
+++ validaya frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Historial from './pages/Historial'
 import Tickets from './pages/Tickets'
 import Perfil from './pages/Perfil'
 import Landing from './pages/Landing'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/tramites" element={<Tramites />} />
         <Route path="/tramite-flow" element={<TramiteFlow />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
